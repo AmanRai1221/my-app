4a8023271bfee7aca9956016100ed483ecd6c83b
AmanRai1221 <amrai858109@gmail.com>
2024-10-07 23:58:48 +0530

    10/07/24

--- src/App.js
+++ src/App.js
@@ -30,24 +30,27 @@ function App() {
       setMode('dark');
       document.body.style.backgroundColor = '#042743';
       showAlert("Dark mode enabled", "success");
-      document.title = 'TextUtils - Dark Mode';
+      // document.title = 'TextUtils - Dark Mode';
     }
     else{
       setMode('light');
       document.body.style.backgroundColor = 'white';
       showAlert("Light mode enabled", "success");
-      document.title = 'TextUtils - Light Mode';
+      // document.title = 'TextUtils - Light Mode';
     }
   }
   return (
-    <>
+    <div>
         <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode} />
         <Alert alert={alert}/>
         <div className="container my-3">
-          <TextForm showAlert={showAlert} heading= "Enter the text to analyze" mode={mode}/>
+          <switch>
+            <about mode={mode} />
+          <TextForm showAlert={showAlert} heading= "Try TextUtils - Word Counter, Character Counter, Copy Text" mode={mode}/>
+          </switch>
           {/* <About/> */}
         </div>
-    </>
+    </div>
   );
 }
 
